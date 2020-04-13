b6441bf12c906d0f6931921eea9e2af6c16b6d0d
musheng <425893642@qq.com>
2020-04-13 13:19:04 +0800

    编辑信息

--- src/api/auth/user.js
+++ src/api/auth/user.js
@@ -1,11 +1,11 @@
 // import request from '@/api/request'
 /**
- * 此处暂时给定登录用户为admin，使用apim中的接口处理登录
+ * 此处暂时给定登录用户为admin
  */
 /**
- * @Description 根据用户名和密码登录
- * @Param {String} username 用户名
- * @Param {String} password 密码
+ * @description 根据用户名和密码登录
+ * @param {String} username 用户名
+ * @param {String} password 密码
  * @return {*} 接口数据
  */
 export function loginByUsername (username, password) {
@@ -22,7 +22,7 @@ export function loginByUsername (username, password) {
 }
 
 /**
- * @Description 退出登录
+ * @description 退出登录
  * @return {*} 接口数据
  */
 export function logout () {
@@ -34,8 +34,8 @@ export function logout () {
 }
 
 /**
- * @Description 根据 token 获取用户信息
- * @Param {String} token
+ * @description 根据 token 获取用户信息
+ * @param {String} token
  * @return {*} 接口数据
  */
 export function getUserInfo (token) {
